b3644ccea7c4d52fa1d40d0a1b84859588190371
DimDrk <dimdrk@gmail.com>
2024-09-29 09:16:22 +0300

    add TeacherUpdateDTO

---
+++ src/main/java/gr/aueb/cf/schoolapp/dto/TeacherUpdateDTO.java
@@ -0,0 +1,26 @@
+package gr.aueb.cf.schoolapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class TeacherUpdateDTO {
+
+    @NotNull
+    private Long id;
+
+    @NotNull
+    @Size(min = 2, max = 255, message = "Το όνομα πρέπει να είναι μεταξύ 2-255 χαρακτήρων.")
+    private String firstName;
+
+    @NotNull
+    @Size(min = 2, max = 255, message = "Το επώνυμο πρέπει να είναι μεταξύ 2-255 χαρακτήρων.")
+    private String lastName;
+}
