9879fce6c3e4822a69689fd4cb070aa67ae7aef2
DimDrk <dimdrk@gmail.com>
2024-09-29 09:29:18 +0300

    add @Provider, @ApplicationScoped on TeacherDAOImpl

--- src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
+++ src/main/java/gr/aueb/cf/schoolapp/dao/TeacherDAOImpl.java
@@ -1,7 +1,11 @@
 package gr.aueb.cf.schoolapp.dao;
 
 import gr.aueb.cf.schoolapp.model.Teacher;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.ext.Provider;
 
+@Provider
+@ApplicationScoped
 public class TeacherDAOImpl extends AbstractDAO<Teacher> implements ITeacherDAO {
 
     public TeacherDAOImpl() {
