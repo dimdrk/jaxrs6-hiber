30d6ec8ad8487225b1326a2f6a50b122e8928a66
DimDrk <dimdrk@gmail.com>
2024-09-29 15:31:20 +0300

    line 13 on ITeacherService edit 'updateTeacher'

--- src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
+++ src/main/java/gr/aueb/cf/schoolapp/service/ITeacherService.java
@@ -2,6 +2,7 @@ package gr.aueb.cf.schoolapp.service;
 
 import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
+import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
 import gr.aueb.cf.schoolapp.service.exceptions.EntityNotFoundException;
 
 import java.util.List;
@@ -9,7 +10,7 @@ import java.util.Map;
 
 public interface ITeacherService {
     TeacherReadOnlyDTO insertTeacher(TeacherInsertDTO insertDTO) throws Exception;
-    TeacherReadOnlyDTO updateTeacher(TeacherReadOnlyDTO updateDTO) throws EntityNotFoundException;
+    TeacherReadOnlyDTO updateTeacher(TeacherUpdateDTO updateDTO) throws EntityNotFoundException;
     void deleteTeacher(Object id) throws EntityNotFoundException;
     TeacherReadOnlyDTO getTeacherById(Object id) throws EntityNotFoundException;
     List<TeacherReadOnlyDTO> getAllTeachers() throws Exception;
