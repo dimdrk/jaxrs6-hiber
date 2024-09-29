39c269d4d20a6362b7fe57e1f7cee46d80be8046
DimDrk <dimdrk@gmail.com>
2024-09-29 15:52:00 +0300

    add ValidatorUtil

---
+++ src/main/java/gr/aueb/cf/schoolapp/validator/ValidatorUtil.java
@@ -0,0 +1,33 @@
+package gr.aueb.cf.schoolapp.validator;
+
+import gr.aueb.cf.schoolapp.model.IdentifiableEntity;
+import jakarta.validation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class ValidatorUtil {
+    private static final Validator validator;
+
+    static {
+        try (ValidatorFactory factory = Validation.buildDefaultValidatorFactory()) {
+            validator = factory.getValidator();
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+
+    private ValidatorUtil() {}
+
+    public static <T extends IdentifiableEntity> List<String> validateDTO(T dto) {
+        Set<ConstraintViolation<T>> violations = validator.validate(dto);
+        List<String> errors = new ArrayList<>();
+        if (!violations.isEmpty()) {
+            for (ConstraintViolation<T> violation : violations) {
+                errors.add(violation.getMessage());
+            }
+        }
+        return errors;
+    }
+}
