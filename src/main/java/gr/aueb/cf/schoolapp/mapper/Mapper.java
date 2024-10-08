1dc7d1b84c79f64543bfb9e9ec8757fd4a4e9cf1
DimDrk <dimdrk@gmail.com>
2024-10-08 22:53:19 +0300

    Mapper updated

--- src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
+++ src/main/java/gr/aueb/cf/schoolapp/mapper/Mapper.java
@@ -5,19 +5,26 @@ import gr.aueb.cf.schoolapp.dto.TeacherReadOnlyDTO;
 import gr.aueb.cf.schoolapp.dto.TeacherUpdateDTO;
 import gr.aueb.cf.schoolapp.model.Teacher;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Mapper {
 
     private Mapper() {}
 
     public static Teacher mapToTeacher(TeacherInsertDTO dto) {
-        return new Teacher(null, dto.getFirstName(), dto.getLastName());
+        return new Teacher(null, dto.getVat(), dto.getFirstName(), dto.getLastName());
     }
 
     public static Teacher mapToTeacher(TeacherUpdateDTO dto) {
-        return new Teacher(dto.getId(), dto.getFirstName(), dto.getLastName());
+        return new Teacher(dto.getId(), dto.getVat(), dto.getFirstName(), dto.getLastName());
     }
 
     public static TeacherReadOnlyDTO mapToTeacherReadOnlyDTO(Teacher teacher) {
-        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getFirstname(), teacher.getLastname());
+        return new TeacherReadOnlyDTO(teacher.getId(), teacher.getVat(), teacher.getFirstname(), teacher.getLastname());
+    }
+
+    public static List<TeacherReadOnlyDTO> teachersToReadOnlyDTOs(List<Teacher> teachers) {
+        return teachers.stream().map(Mapper::mapToTeacherReadOnlyDTO).collect(Collectors.toList());
     }
 }
